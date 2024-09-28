d1b6665971d2bdaff79146605f0876035cd13302
Nirajey-git <nirazgajurel21@gmail.com>
2024-09-28 15:06:43 +1000

    update

--- helpers/axiosHelper.js
+++ helpers/axiosHelper.js
@@ -1,97 +1,98 @@
 import axios from "axios";
-const rootApiEp = "http://localhost:8000/api/v1";
+const rootApiEp =
+    import.meta.env.VITE_ROOT_API
 
 const getAccessJWT = () => {
-  return localStorage.getItem("accessJWT");
+    return localStorage.getItem("accessJWT");
 };
 
-const apiProcessor = async ({ method, url, data, headers }) => {
-  try {
-    const response = await axios({
-      method,
-      url,
-      data,
-      headers,
-    });
+const apiProcessor = async({ method, url, data, headers }) => {
+    try {
+        const response = await axios({
+            method,
+            url,
+            data,
+            headers,
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
+        return response.data;
+    } catch (error) {
+        console.log(error);
 
-    return {
-      status: "error",
-      message: error?.response?.data?.error || error.message,
-    };
-  }
+        return {
+            status: "error",
+            message: error ? .response ? .data ? .error || error.message,
+        };
+    }
 };
 
 // post new user
 export const postNewUser = (data) => {
-  const obj = {
-    method: "post",
-    url: rootApiEp + "/users",
-    data,
-  };
-  return apiProcessor(obj);
+    const obj = {
+        method: "post",
+        url: rootApiEp + "/users",
+        data,
+    };
+    return apiProcessor(obj);
 };
 
 // login user
 export const loginUser = (data) => {
-  const obj = {
-    method: "post",
-    url: rootApiEp + "/users/login",
-    data,
-  };
-  return apiProcessor(obj);
+    const obj = {
+        method: "post",
+        url: rootApiEp + "/users/login",
+        data,
+    };
+    return apiProcessor(obj);
 };
 
 // get user profile
 export const getUser = () => {
-  const obj = {
-    method: "get",
-    url: rootApiEp + "/users",
-    headers: {
-      Authorization: getAccessJWT(),
-    },
-  };
-  return apiProcessor(obj);
+    const obj = {
+        method: "get",
+        url: rootApiEp + "/users",
+        headers: {
+            Authorization: getAccessJWT(),
+        },
+    };
+    return apiProcessor(obj);
 };
 
 // ### transaction api calls
 
 // post new user
 export const postNewTransaction = (data) => {
-  const obj = {
-    method: "post",
-    url: rootApiEp + "/transactions",
-    data,
-    headers: {
-      Authorization: getAccessJWT(),
-    },
-  };
-  return apiProcessor(obj);
+    const obj = {
+        method: "post",
+        url: rootApiEp + "/transactions",
+        data,
+        headers: {
+            Authorization: getAccessJWT(),
+        },
+    };
+    return apiProcessor(obj);
 };
 // fetch transactions for a specific user
 export const fetchTransactions = () => {
-  const obj = {
-    method: "get",
-    url: rootApiEp + "/transactions",
+    const obj = {
+        method: "get",
+        url: rootApiEp + "/transactions",
 
-    headers: {
-      Authorization: getAccessJWT(),
-    },
-  };
-  return apiProcessor(obj);
+        headers: {
+            Authorization: getAccessJWT(),
+        },
+    };
+    return apiProcessor(obj);
 };
 
 export const deleteTransactions = (data) => {
-  const obj = {
-    method: "delete",
-    url: rootApiEp + "/transactions",
-    data,
-    headers: {
-      Authorization: getAccessJWT(),
-    },
-  };
-  return apiProcessor(obj);
+    const obj = {
+        method: "delete",
+        url: rootApiEp + "/transactions",
+        data,
+        headers: {
+            Authorization: getAccessJWT(),
+        },
+    };
+    return apiProcessor(obj);
 };
